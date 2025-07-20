904cc312727cdb7e13ef0af197c9e41eeea96a44
zulfiqar <visiosoft@yahoo.com>
2025-07-20 12:55:14 +0500

    ad

--- backend/src/urls/urls.module.ts
+++ backend/src/urls/urls.module.ts
@@ -3,11 +3,15 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UrlsController } from './urls.controller';
 import { UrlsService } from './urls.service';
 import { Url, UrlSchema } from './schemas/url.schema';
+import { Team, TeamSchema } from '../teams/schemas/team.schema';
 import { AnalyticsModule } from '../analytics/analytics.module';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Url.name, schema: UrlSchema }]),
+    MongooseModule.forFeature([
+      { name: Url.name, schema: UrlSchema },
+      { name: Team.name, schema: TeamSchema }
+    ]),
     AnalyticsModule,
   ],
   controllers: [UrlsController],
